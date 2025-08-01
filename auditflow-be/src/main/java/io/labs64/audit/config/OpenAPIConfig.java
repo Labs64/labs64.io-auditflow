570e585d9b0719a4c2913b53ca7d8b35a87d0420
Alexey Averikhin <alexey.averikhin@labs64.de>
2025-08-01 08:21:31 +0200

    Rollback spring OAuth (will need to do it as Traefik middleware)

--- auditflow-be/src/main/java/io/labs64/audit/config/OpenAPIConfig.java
+++ auditflow-be/src/main/java/io/labs64/audit/config/OpenAPIConfig.java
@@ -7,42 +7,9 @@ import io.swagger.v3.oas.annotations.security.*;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
-        // (1)
         info = @Info(
-                title = "Resource Server Example Service",
-                version = "1.0.0",
-                description = "Documentation for Resource Server Example Service"
-        ),
-        // (2)
-        servers = {
-                @Server(url = "http://nsa2-gateway:8080/resource-server"),
-                @Server(url = "http://nsa2-resource-server-example:8082")
-        }
-)
-@SecuritySchemes({
-        // (3)
-        @SecurityScheme(
-                name = "security-auth",
-                type = SecuritySchemeType.OAUTH2,
-                flows = @OAuthFlows(
-
-                        authorizationCode = @OAuthFlow(
-                                authorizationUrl = "${springdoc.oAuthFlow.authorizationUrl}",
-                                tokenUrl = "${springdoc.oAuthFlow.tokenUrl}",
-                                scopes = {
-                                        @OAuthScope(name = "openid", description = "OpenID"),
-                                        @OAuthScope(name = "profile", description = "Profile")
-                                }
-                        )
-                )
-        ),
-        // (4)
-        @SecurityScheme(
-                name = "bearer-key",
-                type = SecuritySchemeType.HTTP,
-                scheme = "bearer",
-                bearerFormat = "JWT"
+                version = "1.0.0"
         )
-})
+)
 public class OpenAPIConfig {
 }
